eb1eae16dbb5d4f60876334b3d06c6e965c38ab2
Muqtar <tolafejay@gmail.com>
2022-05-12 14:24:57 +0100

    new files

--- trial.py
+++ trial.py
@@ -125,3 +125,74 @@ digits = [1, 3, 4]
 
 for word, digits in zip(word, digits):
     print(word, digits)
+
+
+def cylinder_volume(height, radius):
+    pi = 3.12
+    return height * pi * radius ** 2
+
+print(cylinder_volume(10, 2))
+
+
+
+def buy_eggs():
+    egg_count = 0
+    egg_count += 12 # purchase a dozen eggs
+
+print(buy_eggs())
+
+#lambda
+
+def multiply(x, y):
+    return x * y
+
+print(multiply(3, 5))
+
+# numbers = [
+#               [34, 63, 88, 71, 29],
+#               [90, 78, 51, 27, 45],
+#               [63, 37, 85, 46, 22],
+#               [51, 22, 34, 11, 18]
+#            ]
+#
+# def mean(num_list):
+#     return sum(num_list) / len(num_list)
+#
+# average = list(map(mean,  numbers))
+# print(average)
+
+
+
+def divideNum(number1, number2):
+
+    try:
+        number1 // number2
+    except ZeroDivisionError:
+        print("number cannot be divisible by 0")
+        print("enter valid number")
+
+    return number1 // number2
+
+print(divideNum(10, 2))
+
+
+f = open('arithmetic2.py', 'r')
+file_open = f.read()
+f.close()
+
+
+new_list = []
+sum = 0
+
+for num in range(10):
+    number = int(input("enter two digit number"))
+    new_list.append(number)
+
+    if number % 2 == 0:
+        sum = sum + number
+
+print(new_list)
+
+print("total sum of even number")
+
+print(sum)
